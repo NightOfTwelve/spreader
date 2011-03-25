f0daf8a40403305ecda1d3fc809f9efa77e2053c
loveccx <loveccx@gmail.com>
2011-03-25 13:11:18 +0000

    [Updated] realUser Model

--- spreader/spreader/spreader-runtime/src/main/java/com/nali/spreader/controller/RealUserController.java
+++ spreader/spreader/spreader-runtime/src/main/java/com/nali/spreader/controller/RealUserController.java
@@ -1,5 +1,9 @@
 package com.nali.spreader.controller;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,19 +11,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.nali.spreader.model.RealUser;
+import com.nali.spreader.serialize.XstreamSerializer;
 import com.nali.spreader.user.service.IReadUserService;
 
 @RequestMapping("realuser")
 @Controller
 public class RealUserController {
-
+	
+	private XstreamSerializer serializer = new XstreamSerializer("user", RealUser.class, "users");
+	
 	@Autowired
 	private IReadUserService realUserService;
 	
 	@RequestMapping("save")
 	public void saveRealUsers(HttpServletResponse response, HttpServletRequest request) {
 		// TODO 把xml转换为RealUser对象
-		
+		InputStream ins;
+		try {
+			ins = request.getInputStream();
+			List<RealUser> users = serializer.toBean(ins);
+			realUserService.saveRealUser(users);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
