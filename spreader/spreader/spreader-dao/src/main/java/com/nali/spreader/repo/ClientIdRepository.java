9bb4ae451c5142f2516b033371700c4cc89ed65b
ldd600 <ldd600@gmail.com>
2011-04-01 10:37:21 +0000

    []

---
+++ spreader/spreader/spreader-dao/src/main/java/com/nali/spreader/repo/ClientIdRepository.java
@@ -0,0 +1,21 @@
+package com.nali.spreader.repo;
+
+import java.util.List;
+
+import com.nali.spreader.model.Client;
+
+public class ClientIdRepository extends InmemoryResourceRepository<Client>{
+
+	@Override
+	public Client load(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Client> loadAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ spreader/spreader/spreader-dao/src/main/java/com/nali/spreader/repo/Resource.java
@@ -0,0 +1,5 @@
+package com.nali.spreader.repo;
+
+public interface Resource {
+	Integer getId();
+}
