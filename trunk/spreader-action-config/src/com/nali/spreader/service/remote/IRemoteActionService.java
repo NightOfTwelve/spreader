b0a07b849026313bac7a2e15d167e8311d7e3497
czysay@gmail.com <czysay@gmail.com@5a2aac17-e56e-ac70-9358-26bf2cb0f114>
2011-07-18 09:22:37 +0000

    <added>getActionById

---
+++ trunk/spreader-action-config/src/com/nali/spreader/service/remote/IRemoteActionService.java
@@ -0,0 +1,11 @@
+package com.nali.spreader.service.remote;
+
+import com.nali.spreader.model.ClientAction;
+
+public interface IRemoteActionService {
+	/**
+	 * 查询客户端行为
+	 * @return action和actionStepList，如果不存在则为空
+	 */
+	ClientAction getActionById(Long id);
+}
---
+++ trunk/spreader-action-config/src/com/nali/spreader/service/remote/RemoteActionService.java
@@ -0,0 +1,19 @@
+package com.nali.spreader.service.remote;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nali.spreader.model.ClientAction;
+import com.nali.spreader.service.IActionService;
+
+@Service
+public class RemoteActionService implements IRemoteActionService {
+	@Autowired
+	private IActionService actionService;
+
+	@Override
+	public ClientAction getActionById(Long id) {
+		return actionService.getActionWithStepsById(id);
+	}
+
+}
