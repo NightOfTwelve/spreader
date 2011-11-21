6504979b99f041d4ca0af236b4af732a05a83a10
czysay <czysay@gmail.com>
2011-11-21 05:58:03 +0000

    [added]test for regular job

--- spreader-task-dispatch/src/com/nali/spreader/service/ITaskService.java
+++ spreader-task-dispatch/src/com/nali/spreader/service/ITaskService.java
@@ -1,5 +1,6 @@
 package com.nali.spreader.service;
 
+import java.util.Date;
 import java.util.List;
 
 import com.nali.spreader.model.ClientTask;
@@ -11,12 +12,23 @@ public interface ITaskService {
 
 	List<UserTaskCount> countUserTask();
 
+	/**
+	 * 预分配任务
+	 */
 	void assignToBatch(Integer taskType, Long uid, List<ItemCount<Long>> actionCounts);
 	
 	List<ClientTask> assignBatchTaskToClient(Integer taskType, Long clientId);
 	
 	void reportTask(List<TaskResult> rlts, Integer taskType, Long clientId);
 
+	/**
+	 * 记录已经完成的任务
+	 */
 	void assignToBatch(List<ClientTask> tasks, Integer taskType, Long clientId);
+
+	/**
+	 * 记录未完成的任务
+	 */
+	void assignToBatch(List<ClientTask> tasks, Integer taskType, Date expireTime);
 
 }
