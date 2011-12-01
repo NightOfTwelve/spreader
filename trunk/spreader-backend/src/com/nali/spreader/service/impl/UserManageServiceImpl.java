920a6e20ea4b773f013bce60cb6fcda1ae928de7
eixie9 <eixie9@5a2aac17-e56e-ac70-9358-26bf2cb0f114>
2011-12-01 08:18:56 +0000

    [add]新增查询用户信息

---
+++ trunk/spreader-backend/src/com/nali/spreader/service/impl/UserManageServiceImpl.java
@@ -0,0 +1,10 @@
+package com.nali.spreader.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.nali.spreader.service.IUserManageService;
+
+@Service
+public class UserManageServiceImpl implements IUserManageService {
+
+}
