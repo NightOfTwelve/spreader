767b34e37a75373c050692c8d445a65fd7912532
eixie9 <eixie9@5a2aac17-e56e-ac70-9358-26bf2cb0f114>
2012-01-09 10:13:07 +0000

    <add>增加策略分组保存及检查功能

--- trunk/spreader-task-factory/src/com/nali/spreader/service/impl/StrategyGroupServiceImpl.java
+++ trunk/spreader-task-factory/src/com/nali/spreader/service/impl/StrategyGroupServiceImpl.java
@@ -2,12 +2,17 @@ package com.nali.spreader.service.impl;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nali.common.model.Limit;
 import com.nali.common.pagination.PageResult;
+import com.nali.spreader.dao.ICrudRegularJobDao;
 import com.nali.spreader.dao.ICrudStrategyGroupDao;
+import com.nali.spreader.dao.IStrategyGroupDao;
+import com.nali.spreader.model.RegularJob;
+import com.nali.spreader.model.RegularJobExample;
 import com.nali.spreader.model.StrategyGroup;
 import com.nali.spreader.model.StrategyGroupExample;
 import com.nali.spreader.model.StrategyGroupExample.Criteria;
@@ -20,8 +25,14 @@ import com.nali.spreader.service.IStrategyGroupService;
  */
 @Service
 public class StrategyGroupServiceImpl implements IStrategyGroupService {
+	private static final Logger logger = Logger
+			.getLogger(StrategyGroupServiceImpl.class);
 	@Autowired
 	private ICrudStrategyGroupDao gcrudDao;
+	@Autowired
+	private IStrategyGroupDao groupDao;
+	@Autowired
+	private ICrudRegularJobDao crudRegDao;
 
 	@Override
 	public PageResult<StrategyGroup> findStrategyGroupPageResult(
@@ -46,4 +57,39 @@ public class StrategyGroupServiceImpl implements IStrategyGroupService {
 		return new PageResult<StrategyGroup>(list, lit, cnt);
 	}
 
+	@Override
+	public Long saveGroupInfo(StrategyGroup sg) {
+		Long key = null;
+		if (sg != null) {
+			key = groupDao.insertReturnKey(sg);
+		} else {
+			logger.warn("StrategyGroup为null,无法保存");
+		}
+		return key;
+	}
+
+	@Override
+	public void syncRegularJob(Long gid, Long regularJobId) {
+		if (!checkRegularJobGroupId(gid, regularJobId)) {
+			RegularJob rj = crudRegDao.selectByPrimaryKey(regularJobId);
+			if (rj != null) {
+				rj.setGid(gid);
+				crudRegDao.updateByPrimaryKeyWithBLOBs(rj);
+			} else {
+				logger.warn("获取RegularJob失败");
+			}
+		}
+	}
+
+	@Override
+	public Boolean checkRegularJobGroupId(Long gid, Long regId) {
+		Boolean tag = false;
+		if (gid != null && regId != null) {
+			RegularJob rj = crudRegDao.selectByPrimaryKey(regId);
+			if (rj != null) {
+				tag = rj.getGid().equals(gid);
+			}
+		}
+		return tag;
+	}
 }
