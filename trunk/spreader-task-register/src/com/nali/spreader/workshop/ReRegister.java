3c9c0c856d36b839d06bfafdb02ba215addc817a
czysay@gmail.com <czysay@gmail.com@5a2aac17-e56e-ac70-9358-26bf2cb0f114>
2012-01-11 09:29:19 +0000

    [fixed]

--- trunk/spreader-task-register/src/com/nali/spreader/workshop/ReRegister.java
+++ trunk/spreader-task-register/src/com/nali/spreader/workshop/ReRegister.java
@@ -3,13 +3,14 @@ package com.nali.spreader.workshop;
 import org.springframework.stereotype.Component;
 
 import com.nali.spreader.factory.TaskProduceLine;
+import com.nali.spreader.factory.config.SystemObject;
 import com.nali.spreader.factory.config.desc.ClassDescription;
 import com.nali.spreader.factory.passive.AutowireProductLine;
 import com.nali.spreader.factory.regular.RegularAnalyzer;
 
 @Component
 @ClassDescription("重新注册帐号")
-public class ReRegister implements RegularAnalyzer {//TODO temp code
+public class ReRegister implements RegularAnalyzer, SystemObject {//TODO temp code
 	@AutowireProductLine
 	private TaskProduceLine<Object> reRegisterPassive;
 
