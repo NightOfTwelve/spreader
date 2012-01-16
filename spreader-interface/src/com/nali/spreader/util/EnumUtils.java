a9d619525dec2059546f42eb76b3509f4254d4dd
ldd600 <ldd600@gmail.com>
2012-01-16 07:57:15 +0000

    [sprader interface updated for enums]

--- spreader-interface/src/com/nali/spreader/util/EnumUtils.java
+++ spreader-interface/src/com/nali/spreader/util/EnumUtils.java
@@ -1,10 +1,15 @@
 package com.nali.spreader.util;
 
+import com.nali.lang.StringUtils;
+
 public class EnumUtils {
   private EnumUtils() {
   }
   
   public static boolean matchEnum(String match, int value, String name, String...descriptions) {
+	  if(StringUtils.isEmptyNoOffset(match)) {
+		  throw new IllegalArgumentException("Match string is null or empty!");
+	  }
 	  if(!String.valueOf(value).equals(match)) {
 		  if(!name.equalsIgnoreCase(match)) {
 			  for(String description : descriptions) {
