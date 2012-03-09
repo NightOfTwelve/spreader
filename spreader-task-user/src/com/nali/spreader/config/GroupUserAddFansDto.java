d8fb83bf98013e05a0a0dbf776a60442f337789a
eixie9 <eixie9@5a2aac17-e56e-ac70-9358-26bf2cb0f114>
2012-03-09 09:22:32 +0000

    <add>用户分组关注

---
+++ spreader-task-user/src/com/nali/spreader/config/GroupUserAddFansDto.java
@@ -0,0 +1,39 @@
+package com.nali.spreader.config;
+
+import java.io.Serializable;
+
+import com.nali.spreader.factory.config.desc.PropertyDescription;
+
+public class GroupUserAddFansDto implements Serializable {
+	private static final long serialVersionUID = 4995588216370590441L;
+	@PropertyDescription("最大关注人数")
+	private Integer maxUserValue;
+	@PropertyDescription("最小关注人数")
+	private Integer minUserValue;
+//	@PropertyDescription("机器人比例")
+//	private Double robotRate;
+
+	public Integer getMaxUserValue() {
+		return maxUserValue;
+	}
+
+	public void setMaxUserValue(Integer maxUserValue) {
+		this.maxUserValue = maxUserValue;
+	}
+
+	public Integer getMinUserValue() {
+		return minUserValue;
+	}
+
+	public void setMinUserValue(Integer minUserValue) {
+		this.minUserValue = minUserValue;
+	}
+
+	// public Double getRobotRate() {
+	// return robotRate;
+	// }
+	//
+	// public void setRobotRate(Double robotRate) {
+	// this.robotRate = robotRate;
+	// }
+}
