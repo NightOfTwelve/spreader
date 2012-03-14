6bc8346fddf283bfbae0358fb8afccc352361510
czysay@gmail.com <czysay@gmail.com@5a2aac17-e56e-ac70-9358-26bf2cb0f114>
2012-03-14 07:21:55 +0000

    <added>分组机器人转发

---
+++ trunk/spreader-task-user/src/com/nali/spreader/service/IRobotContentService.java
@@ -0,0 +1,27 @@
+package com.nali.spreader.service;
+
+import java.util.List;
+
+import com.nali.spreader.model.RobotContent;
+
+/**
+ * IRobotContentService<br>&nbsp;
+ * 
+ * @author sam Created on 2012-3-14
+ */
+public interface IRobotContentService {
+	/**
+	 * 生成或更新status，并且补全其他字段
+	 * @see RobotContent#STATUS_CREATE
+	 * @see RobotContent#STATUS_DONE
+	 */
+	void save(Long robotId, Long contentId, Integer status);
+	
+	/**
+	 * 根据帖子id查找相关机器人id，type传空表示类型不限
+	 * @see RobotContent#TYPE_POST
+	 * @see RobotContent#TYPE_FORWARD
+	 * @see RobotContent#TYPE_REPLY
+	 */
+	List<Long> findRelatedRobotId(Long contentId, Integer type);
+}
