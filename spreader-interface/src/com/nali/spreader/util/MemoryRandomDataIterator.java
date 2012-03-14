d12b7f97797fb2fc9114c467567973a5f2b1057d
ldd600 <ldd600@gmail.com>
2012-03-14 05:43:49 +0000

    [updated uidset]

---
+++ spreader-interface/src/com/nali/spreader/util/MemoryRandomDataIterator.java
@@ -0,0 +1,50 @@
+package com.nali.spreader.util;
+
+import java.lang.reflect.Array;
+import java.util.List;
+import java.util.Set;
+
+import com.nali.spreader.util.random.RandomUtil;
+
+public abstract class MemoryRandomDataIterator<T, E> extends DataIterator<E>{
+	private T[] randomIds;
+	
+	public MemoryRandomDataIterator(long upperCount, int batchSize) {
+		super(0, batchSize);
+		if(upperCount < 0) {
+			throw new IllegalArgumentException("Ilelgal rand count: " + upperCount);
+		}
+		
+		List<T> ids = this.queryAllIds();
+		Set<T> excludeIds = this.getExcludeIds();
+		int tempCount = ids.size() - excludeIds.size();
+		if(tempCount <= upperCount) {
+			this.count = tempCount;
+		}else{
+			this.count = upperCount;
+			ids = RandomUtil.randomItems(ids, (int)upperCount);
+		}
+		
+		
+		if(ids.size() > 0) {
+			T t = ids.get(0);
+			this.randomIds = (T[]) Array.newInstance(t.getClass(), ids.size());
+		}
+	}
+	
+	protected abstract List<T> queryAllIds();
+	
+	protected abstract Set<T> getExcludeIds();
+	
+	protected abstract List<E> queryElements(T[] ids);
+	
+	@Override
+	protected List<E> query(long offset, int limit) {
+		T[] rtnIds =   (T[])java.lang.reflect.Array.newInstance(
+               this.randomIds.getClass().getComponentType(), limit);
+	    System.arraycopy(randomIds, (int)offset, rtnIds, 0, limit);
+	    
+	    List<E> elements = this.queryElements(rtnIds);
+	    return elements;
+	}
+}
