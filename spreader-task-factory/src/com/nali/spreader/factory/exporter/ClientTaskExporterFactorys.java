49859811254bcd663af8fcd9c6e53c6ad0b59c2b
czysay <czysay@gmail.com>
2012-03-29 09:50:13 +0000

    <updated>add channel 104

--- spreader-task-factory/src/com/nali/spreader/factory/exporter/ClientTaskExporterFactorys.java
+++ spreader-task-factory/src/com/nali/spreader/factory/exporter/ClientTaskExporterFactorys.java
@@ -5,41 +5,42 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import com.nali.lwtmq.sender.AsyncSender;
-import com.nali.spreader.constants.TaskType;
 import com.nali.spreader.model.ClientTask;
 import com.nali.spreader.service.ITaskService;
 
 @Component
 public class ClientTaskExporterFactorys {
-	private MultiLwtmqTaskSender passiveTaskSender = new MultiLwtmqTaskSender();
+//	private MultiLwtmqTaskSender passiveTaskSender = new MultiLwtmqTaskSender();
 	@Autowired
 	private ITaskService taskService;
 	@Autowired
 	private ThreadLocalResultInfo threadLocalResultInfo;
+//
+//	@Autowired
+//	public void setWeiboNormalPassiveTaskSender(AsyncSender<ClientTask> weiboNormalPassiveTaskSender) {
+//		for (TaskType tt : TaskType.values()) {
+//			passiveTaskSender.register(tt.getId(), weiboNormalPassiveTaskSender);
+//		}
+//	}
+//
+//	@Autowired
+//	public void setWeiboRegisterPassiveTaskSender(AsyncSender<ClientTask> weiboRegisterPassiveTaskSender) {
+//		passiveTaskSender.register(TaskType.weiboRegister.getId(), weiboRegisterPassiveTaskSender);
+//	}
+//	
+//	@Autowired
+//	public void setWeiboInstantPassiveTaskSender(AsyncSender<ClientTask> weiboInstantPassiveTaskSender) {
+//		passiveTaskSender.register(TaskType.weiboInstant.getId(), weiboInstantPassiveTaskSender);
+//	}
+//	
+//	@Bean
+//	public ClientTaskExporterFactory passiveTaskExporterFactory() {//TODO 减少queue配置
+//		return new ClientTaskExporterFactory(taskService, passiveTaskSender, threadLocalResultInfo);
+//	}
 
-	@Autowired
-	public void setWeiboNormalPassiveTaskSender(AsyncSender<ClientTask> weiboNormalPassiveTaskSender) {
-		passiveTaskSender.register(TaskType.weiboNormal.getId(), weiboNormalPassiveTaskSender);
-	}
-
-	@Autowired
-	public void setWeiboRegisterPassiveTaskSender(AsyncSender<ClientTask> weiboRegisterPassiveTaskSender) {
-		passiveTaskSender.register(TaskType.weiboRegister.getId(), weiboRegisterPassiveTaskSender);
-	}
-	
-	@Autowired
-	public void setWeiboInstantPassiveTaskSender(AsyncSender<ClientTask> weiboInstantPassiveTaskSender) {
-		passiveTaskSender.register(TaskType.weiboInstant.getId(), weiboInstantPassiveTaskSender);
-	}
-	
 	@Bean
-	public ClientTaskExporterFactory passiveTaskExporterFactory() {//TODO 减少queue配置
-		return new ClientTaskExporterFactory(taskService, passiveTaskSender, threadLocalResultInfo);
-	}
-
-	@Bean
 	@Autowired
-	public ClientTaskExporterFactory regularTaskExporterFactory(AsyncSender<ClientTask> regularTaskSender) {
+	public ClientTaskExporterFactory clientTaskExporterFactory(AsyncSender<ClientTask> regularTaskSender) {
 		LwtmqTaskSender lwtmqTaskSender = new LwtmqTaskSender();
 		lwtmqTaskSender.setLwtmqSender(regularTaskSender);
 		return new ClientTaskExporterFactory(taskService, lwtmqTaskSender, threadLocalResultInfo);
