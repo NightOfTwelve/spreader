115919824ff5f7b72483e1543dcb39ca80de3ecb
czysay <czysay@gmail.com>
2012-05-24 07:19:15 +0000

    [fixed]

--- spreader-backend/db/update-2012-04-23/update.sql
+++ spreader-backend/db/update-2012-04-23/update.sql
@@ -3,8 +3,10 @@ create table tb_keyword
    id                   bigint not null auto_increment,
    name                 varchar(50),
    category_id          bigint,
-   tag                  bit(0) not null default 0,
+   tag                  bit(0) not null default 0 comment '是否是手工建的关键字',
    create_time          timestamp,
+   executable			bit(0) not null default true comment '执行状态，false表示被锁定',
+   allowtag				bit(0) default true comment '是否可做标签关键字，true可以做标签',
    primary key (id)
 )
 auto_increment = 800000
@@ -14,27 +16,20 @@ COLLATE = utf8_bin;
 
 alter table tb_keyword comment '关键字表';
 
-create unique index idx_keyword_name on tb_keyword
+create unique index uk_keyword_name on tb_keyword
 (
    name
 );
 
 insert into tb_keyword
-select id,name,id,false,current_timestamp() From tb_category;
+select id,name,id,false,current_timestamp(),true,true from tb_category;
 
-ALTER TABLE tb_user_tag ADD tag_id bigint;
 
-update tb_user_tag
-set tag_id = category_id;
-
-alter table tb_keyword add column executable bit(0) not null default true ;
-
-DROP INDEX index_name ON talbe_name
 
 DROP INDEX uk_category_user ON tb_user_tag;
 
-ALTER TABLE tb_user_tag ADD INDEX idx_tag_user (uid,tag_id);
-
 alter table tb_user_tag drop column category_id;
 
-alter table tb_keyword add column allowtag bit(0) default true ;
+ALTER TABLE tb_user_tag ADD tag_id bigint;
+
+ALTER TABLE tb_user_tag ADD INDEX idx_tag_user (uid,tag_id);
