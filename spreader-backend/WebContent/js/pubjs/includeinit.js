3d3408bc920df03dfeef4789f1fffcab4b81dab9
eixie9 <eixie9@5a2aac17-e56e-ac70-9358-26bf2cb0f114>
2012-08-13 06:36:00 +0000

    <add>增加my97整合日历插件

--- spreader-backend/WebContent/js/pubjs/includeinit.js
+++ spreader-backend/WebContent/js/pubjs/includeinit.js
@@ -1,3 +1,19 @@
 Ext.BLANK_IMAGE_URL = $CONFIG.BLANK_IMAGE_URL;
 Ext.chart.Chart.CHART_URL = $CONFIG.CHART_URL;
 Ext.QuickTips.init();
+var host = 'http://' + window.location.host;
+/**
+ * 捕获ajax异常做重定向处理 登录超时
+ */
+Ext.Ajax.on('requestexception', function(conn, response, options) {
+			if (response.status == '401') {
+				setTimeout(function() {
+							// 延时避免被failure回调函数中的aler覆盖
+							top.Ext.MessageBox.alert('提示', '会话链接超时，请重新登录',
+									function() {
+										parent.location.href = host
+												+ '/spreader-backend/account/init';
+									});
+						}, 200);
+			}
+		});
