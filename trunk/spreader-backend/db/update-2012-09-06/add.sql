0d6d4a3c159b01856ef602a5bfc1ac769b1b9115
eixie9 <eixie9@5a2aac17-e56e-ac70-9358-26bf2cb0f114>
2012-09-11 07:34:14 +0000

    <add>客户端IP记录表

---
+++ trunk/spreader-backend/db/update-2012-09-06/add.sql
@@ -0,0 +1,19 @@
+create table spreader.tb_ip_record
+(
+   id                   bigint not null auto_increment,
+   client_id            bigint not null comment '客户端编号',
+   ip            		varchar(20) not null comment 'IP',
+   create_time 			timestamp not null comment 'IP开始登记时间',
+   record_time          timestamp not null comment '登记时间',
+   primary key (id)
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
+
+alter table spreader.tb_ip_record comment '客户端IP记录';
+
+create index idx_client_id_create_time on spreader.tb_ip_record
+(
+   client_id,create_time
+);
