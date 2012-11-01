c7af7cc2b5723fe48101403817b04e5b933db2b4
eixie9 <eixie9@5a2aac17-e56e-ac70-9358-26bf2cb0f114>
2012-11-01 04:35:33 +0000

    <update>重构用户分组

---
+++ trunk/spreader-task-factory/src/com/nali/spreader/config/Range.java
@@ -0,0 +1,32 @@
+package com.nali.spreader.config;
+
+import java.io.Serializable;
+
+import com.nali.spreader.factory.config.desc.PropertyDescription;
+
+public class Range<T extends Comparable<?>> implements Serializable {
+	private static final long serialVersionUID = 2145619732293369346L;
+	@PropertyDescription("小等于")
+	private T lte;
+	@PropertyDescription("大等于")
+	private T gte;
+	public T getGte() {
+		return gte;
+	}
+	public void setGte(T gte) {
+		this.gte = gte;
+	}
+	public T getLte() {
+		return lte;
+	}
+	public void setLte(T lte) {
+		this.lte = lte;
+	}
+	public boolean checkNotNull() {
+		return gte!=null && lte!=null;
+	}
+
+	public boolean checkAllNull() {
+		return gte == null && lte == null;
+	}
+}
