248d8a43eaf6f12e73dca80706a71b490399b555
eixie9 <eixie9@5a2aac17-e56e-ac70-9358-26bf2cb0f114>
2012-11-06 09:56:09 +0000

    <update>更新用户分组功能

--- trunk/spreader-interface/src/com/nali/spreader/util/DependencyAnalyzer.java
+++ trunk/spreader-interface/src/com/nali/spreader/util/DependencyAnalyzer.java
@@ -14,26 +14,27 @@ public class DependencyAnalyzer<T> {
 	private static final List<Object> ASSEMBLED = Collections.emptyList();
 	private Map<T, List<T>> dependsMap;
 	private Set<T> assembling = new HashSet<T>();
-	
+
 	public DependencyAnalyzer(Map<T, List<T>> dependsMap) {
 		super();
 		this.dependsMap = dependsMap;
 	}
-	
+
 	public static <T> List<T> getDependsOrder(T toCheck, Map<T, List<T>> dependsMap) {
-		return new DependencyAnalyzer<T>(new HashMap<T, List<T>>(dependsMap)).getDependsOrder(toCheck);
+		return new DependencyAnalyzer<T>(new HashMap<T, List<T>>(dependsMap))
+				.getDependsOrder(toCheck);
 	}
-	
+
 	public static <T> List<T> getDependsOrder(Map<T, List<T>> dependsMap) {
 		return new DependencyAnalyzer<T>(new HashMap<T, List<T>>(dependsMap)).getDependsOrder();
 	}
-	
+
 	public List<T> getDependsOrder(T toCheck) {
 		List<T> dependsOrder = new ArrayList<T>();
 		checkDepends(toCheck, dependsOrder);
 		return dependsOrder;
 	}
-	
+
 	public List<T> getDependsOrder() {
 		List<T> checkOrder = new ArrayList<T>(dependsMap.keySet());
 		List<T> dependsOrder = new ArrayList<T>();
@@ -42,32 +43,32 @@ public class DependencyAnalyzer<T> {
 		}
 		return dependsOrder;
 	}
-	
+
 	void checkDepends(T start, List<T> dependsOrder) {
 		List<T> error = checkDepends0(start, dependsOrder);
-		if(error!=null) {
+		if (error != null) {
 			throw new AssemblingException(error);
 		}
 	}
 
 	@SuppressWarnings("unchecked")
 	List<T> checkDepends0(T start, List<T> dependsOrder) {
-		if(assembling.contains(start)) {
+		if (assembling.contains(start)) {
 			List<T> error = new LinkedList<T>();
 			error.add(start);
 			return error;
 		}
 		List<T> depends = dependsMap.get(start);
-		if(isAssembled(depends)) {
-			//do nothing
-		} else if(isNoDepends(depends)) {
+		if (isAssembled(depends)) {
+			// do nothing
+		} else if (isNoDepends(depends)) {
 			dependsMap.put(start, (List<T>) ASSEMBLED);
 			dependsOrder.add(start);
 		} else {
 			assembling.add(start);
 			for (T depend : depends) {
 				List<T> error = checkDepends0(depend, dependsOrder);
-				if(error!=null) {
+				if (error != null) {
 					error.add(0, start);
 					return error;
 				}
@@ -78,44 +79,51 @@ public class DependencyAnalyzer<T> {
 		}
 		return null;
 	}
-	
+
 	private boolean isNoDepends(List<T> depends) {
-		return depends==null;
+		return depends == null;
 	}
 
 	private boolean isAssembled(List<T> depends) {
-		return depends!=null && depends.isEmpty();
+		return depends != null && depends.isEmpty();
 	}
 
 	public static class AssemblingException extends RuntimeException {
 		private static final long serialVersionUID = 5894139053601243001L;
+
 		public AssemblingException(String message) {
 			super(message);
 		}
+
 		public AssemblingException(List<?> errors) {
 			this(getMessage(errors));
 		}
-		
+
 		private static String getMessage(List<?> errors) {
 			StringBuilder sb = new StringBuilder();
 			for (Iterator<?> iterator = errors.iterator(); iterator.hasNext();) {
 				Object error = iterator.next();
 				sb.append(error);
-				if(iterator.hasNext()) {
+				if (iterator.hasNext()) {
 					sb.append("-->");
 				}
 			}
 			return sb.toString();
 		}
 	}
-	
-//	public static void main(String[] args) {
-//		Map<String,List<String>> map = new HashMap<String, List<String>>();
-//		map.put("A", Arrays.asList("B", "C"));
-//		map.put("B", Arrays.asList("C", "D"));
-//		System.out.println(getDependsOrder(map));
-//		System.out.println(getDependsOrder("B", map));
-//		map.put("D", Arrays.asList("C", "A"));
-//		System.out.println(getDependsOrder("B", map));
-//	}
+
+	// public static void main(String[] args) {
+	// Map<String,List<String>> map = new HashMap<String, List<String>>();
+	// map.put("A", Arrays.asList("B", "C"));
+	// map.put("B", Arrays.asList("C", "D"));
+	// // DependencyAnalyzer<String> da = new DependencyAnalyzer<String>(map);
+	// // System.out.println(da.getDependsOrder());
+	// System.out.println(getDependsOrder(map));
+	// Map<String,List<String>> map2 = new HashMap<String, List<String>>();
+	// System.out.println(getDependsOrder("B",
+	// Collections.<String,List<String>>emptyMap()));
+	// // map.put("D", Arrays.asList("C", "A"));
+	// // DependencyAnalyzer<String> d = new DependencyAnalyzer<String>(map);
+	// // System.out.println(d.getDependsOrder("B"));
+	// }
 }
