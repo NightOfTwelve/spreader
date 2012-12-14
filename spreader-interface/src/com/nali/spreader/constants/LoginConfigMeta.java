4c76d0c6f37361b26d9b3440d46d9a43bdd8c8ed
eixie9 <eixie9@5a2aac17-e56e-ac70-9358-26bf2cb0f114>
2012-12-14 10:08:01 +0000

    <add>增加一些枚举，修改平均分配的工具类

--- spreader-interface/src/com/nali/spreader/constants/LoginConfigMeta.java
+++ spreader-interface/src/com/nali/spreader/constants/LoginConfigMeta.java
@@ -2,23 +2,68 @@ package com.nali.spreader.constants;
 
 import java.util.Map;
 
+import com.nali.common.util.CollectionUtils;
 import com.nali.spreader.model.RobotUser;
 
 public enum LoginConfigMeta {
-	weibo(1, 1L) {
+	weibo(1, 3L) {
 		@Override
 		public Map<String, Object> getLoginParams(RobotUser robotUser) {
-			// TODO Auto-generated method stub
-			return null;
+			Map<String, Object> contentObjects = CollectionUtils.newHashMap(2);
+			contentObjects.put("name", robotUser.getLoginName());
+			contentObjects.put("password", robotUser.getLoginPwd());
+			return contentObjects;
 		}
 	},
-	;
-	public final Integer websiteId;
-	public final Long actionId;
+	apple(2, 2003L) {
+		@Override
+		public Map<String, Object> getLoginParams(RobotUser robotUser) {
+			Map<String, Object> contentObjects = CollectionUtils.newHashMap(2);
+			String[] loginNames = robotUser.getLoginName().split("\\#");
+			contentObjects.put("name", loginNames[0]);
+			contentObjects.put("udid", loginNames[1]);
+			contentObjects.put("password", robotUser.getLoginPwd());
+			return contentObjects;
+		}
+	},
+	ximalaya(3, 3003L) {
+		@Override
+		public Map<String, Object> getLoginParams(RobotUser robotUser) {
+			Map<String, Object> contentObjects = CollectionUtils.newHashMap(2);
+			contentObjects.put("name", robotUser.getLoginName());
+			contentObjects.put("password", robotUser.getLoginPwd());
+			return contentObjects;
+		}
+	};
+	private static Map<Integer, LoginConfigMeta> loginConfigMap;
+	private final Integer websiteId;
+	private final Long actionId;
+
+	static {
+		LoginConfigMeta[] configs = LoginConfigMeta.values();
+		loginConfigMap = CollectionUtils.newHashMap(configs.length);
+		for (LoginConfigMeta config : configs) {
+			loginConfigMap.put(config.getWebsiteId(), config);
+		}
+	}
+
+	public static Map<Integer, LoginConfigMeta> getLoginConfigMap() {
+		return loginConfigMap;
+	}
+
+	public Integer getWebsiteId() {
+		return websiteId;
+	}
+
+	public Long getActionId() {
+		return actionId;
+	}
+
 	private LoginConfigMeta(Integer websiteId, Long actionId) {
 		this.websiteId = websiteId;
 		this.actionId = actionId;
 	}
+
 	public abstract Map<String, Object> getLoginParams(RobotUser robotUser);
-	
+
 }
