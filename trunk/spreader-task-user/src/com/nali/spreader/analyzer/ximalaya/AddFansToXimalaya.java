1e9a2fd5cb5582b2888b6199db2a4a7d48d01165
eixie9 <eixie9@5a2aac17-e56e-ac70-9358-26bf2cb0f114>
2013-01-21 06:11:15 +0000

    <update>改进喜马拉雅加粉策略

--- trunk/spreader-task-user/src/com/nali/spreader/analyzer/ximalaya/AddFansToXimalaya.java
+++ trunk/spreader-task-user/src/com/nali/spreader/analyzer/ximalaya/AddFansToXimalaya.java
@@ -1,16 +1,14 @@
 package com.nali.spreader.analyzer.ximalaya;
 
 import java.io.Serializable;
+import java.util.Collections;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.apache.commons.lang.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.nali.common.util.CollectionUtils;
 import com.nali.spreader.constants.Website;
 import com.nali.spreader.data.User;
 import com.nali.spreader.data.UserRelation;
@@ -26,7 +24,6 @@ import com.nali.spreader.service.IUserGroupFacadeService;
 import com.nali.spreader.util.avg.Average;
 import com.nali.spreader.util.avg.AverageHelper;
 import com.nali.spreader.util.avg.ItemCount;
-import com.nali.spreader.util.random.RandomUtil;
 import com.nali.spreader.workshop.other.AddUserFans;
 import com.nali.spreader.workshop.ximalaya.AddXimalayaFans.AddXimalayaWorkDto;
 
@@ -60,44 +57,39 @@ public class AddFansToXimalaya extends UserGroupExtendedBeanImpl implements
 		List<Long> toUids = globalUserService.getFansLimitUids(
 				Website.ximalaya.getId(),
 				userGroupFacadeService.getUids(toGid), fansLimit);
-		Integer toAddUserCount = toUids.size();
-		List<ItemCount<Long>> execuData = AverageHelper.getItemCounts(
-				execuAddLimit, fromUids);
-		Average<Long> avg = AverageHelper.selectAverageByParam(toAddUserCount,
-				fromUids.size(), addLimit, execuAddLimit, execuData);
-		Date addTime = new Date();
-		Set<Long> addExists = new HashSet<Long>();
-		while (avg.hasNext()) {
-			List<ItemCount<Long>> items = avg.next();
-			List<Long> workData = RandomUtil.randomItems(toUids, addExists, 1);
-			if (!CollectionUtils.isEmpty(workData)) {
-				Long toUid = workData.get(0);
-				User toUser = globalUserService.getUserById(toUid);
-				// 获取toUid所有的粉丝
-				List<Long> existsFans = globalUserService.findRelationUserId(
-						toUid, UserRelation.TYPE_ATTENTION,
-						Website.ximalaya.getId(), toUser.getIsRobot());
-				Set<Long> existsFansSet = new HashSet<Long>(existsFans);
+		Collections.shuffle(toUids);
+		for (Long toUid : toUids) {
+			User toUser = globalUserService.getUserById(toUid);
+			// 获取toUid已有的粉丝
+			List<Long> existsFans = globalUserService.findRelationUserId(toUid,
+					UserRelation.TYPE_ATTENTION, Website.ximalaya.getId(),
+					toUser.getIsRobot());
+			// 粉丝要和机器人组做一次排重
+			@SuppressWarnings("unchecked")
+			List<Long> noExistsFans = (List<Long>) org.apache.commons.collections.CollectionUtils
+					.subtract(fromUids, existsFans);
+			List<ItemCount<Long>> execuData = AverageHelper.getItemCounts(
+					execuAddLimit, noExistsFans);
+			Average<Long> avg = AverageHelper.selectAverageByParam(
+					toUids.size(), noExistsFans.size(), addLimit,
+					execuAddLimit, execuData);
+			Date addTime = new Date();
+			while (avg.hasNext()) {
+				List<ItemCount<Long>> items = avg.next();
 				User u = globalUserService.getUserById(toUid);
 				long fans = u.getFans();
 				for (ItemCount<Long> item : items) {
 					Long fromUid = item.getItem();
 					int count = item.getCount();
-					if (fans <= fansLimit) {
+					if (fansLimit > fans) {
 						if (count > 0) {
-							// 排重
-							if (!existsFansSet.contains(fromUid)) {
-								addFans(fromUid, toUid, addTime);
-								addTime = DateUtils.addMinutes(addTime,
-										execuInterval);
-								fans++;
-							}
+							addFans(fromUid, toUid, addTime);
+							addTime = DateUtils.addMinutes(addTime,
+									execuInterval);
+							fans++;
 						}
-					} else {
-						break;
 					}
 				}
-				addExists.add(toUid);
 			}
 		}
 		return null;
@@ -143,9 +135,9 @@ public class AddFansToXimalaya extends UserGroupExtendedBeanImpl implements
 		private Long attentionLimit;
 		@PropertyDescription("被关注人的粉丝数上限(超过此上限该用户不再添加粉丝,不填则不受限)")
 		private Long fansLimit;
-		@PropertyDescription("微博作者被关注的次数上限(单次)")
+		@PropertyDescription("单个账户被加粉次数上限(单次)")
 		private Integer addCount;
-		@PropertyDescription("机器人执行关注的次数上限(单次)")
+		@PropertyDescription("机器人执行关注的次数上限(单次策略)")
 		private Integer execuAddCount;
 
 		public Integer getExecuInterval() {
