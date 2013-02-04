cb3a41059b8b063602dbdf206fb133780fcba7ad
eixie9 <eixie9@5a2aac17-e56e-ac70-9358-26bf2cb0f114>
2013-02-04 09:29:41 +0000

    <add>分词接口

---
+++ trunk/spreader-interface/src/com/nali/spreader/remote/ISegmenAnalyzerService.java
@@ -0,0 +1,53 @@
+package com.nali.spreader.remote;
+
+import java.util.List;
+import java.util.Set;
+
+public interface ISegmenAnalyzerService {
+	/**
+	 * 根据分析器获取内容的分词结果
+	 * 
+	 * @param content
+	 * @return
+	 */
+	Set<String> getContentSegmen(String content);
+
+	/**
+	 * 获取一个分词ID
+	 * 
+	 * @param segmen
+	 * @return
+	 */
+	Long assignContentSegmenId(String segmen);
+
+	/**
+	 * 对reply索引 以后优化成增量索引
+	 * 
+	 * @param lastId
+	 */
+	void createReplyIndex();
+
+	/**
+	 * 保存一条回复记录并返回ID
+	 * 
+	 * @param reply
+	 * @return
+	 */
+	Long assignReplayId(String reply);
+
+	/**
+	 * 建立索引
+	 * 
+	 * @param weibo
+	 * @param comments
+	 */
+	void execuAnalysisSegmen(String weibo, List<String> comments);
+
+	/**
+	 * 根据微博匹配最相似的回复内容
+	 * 
+	 * @param weibo
+	 * @return
+	 */
+	String getWeiboSearchKeywords(String weibo);
+}
