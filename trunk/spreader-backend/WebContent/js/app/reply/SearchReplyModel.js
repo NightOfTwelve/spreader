974cbe99ff2c8832f1f356ea2c83be4c5787a935
eixie9 <eixie9@5a2aac17-e56e-ac70-9358-26bf2cb0f114>
2013-02-17 07:02:00 +0000

    <add>增加维护微博cookies的功能

--- trunk/spreader-backend/WebContent/js/app/reply/SearchReplyModel.js
+++ trunk/spreader-backend/WebContent/js/app/reply/SearchReplyModel.js
@@ -1,99 +1,136 @@
 Ext.onReady(function() {
-			var store = new Ext.data.Store({
-						proxy : new Ext.data.HttpProxy({
-									url : '../reply/search?_time='
-											+ new Date().getTime()
-								}),
-						reader : new Ext.data.JsonReader({
-									root : 'list'
-								}, [{
-											name : 'reply'
-										}, {
-											name : 'score'
-										}])
-					});
+	var store = new Ext.data.Store({
+				proxy : new Ext.data.HttpProxy({
+							url : '../reply/search?_time='
+									+ new Date().getTime()
+						}),
+				reader : new Ext.data.JsonReader({
+							root : 'list'
+						}, [{
+									name : 'reply'
+								}, {
+									name : 'score'
+								}])
+			});
 
-			// 定义Checkbox
-			var sm = new Ext.grid.CheckboxSelectionModel({
-						singleSelect : true
-					});
-			// 定义自动当前页行号
-			var rm = new Ext.grid.RowNumberer({
-						header : 'NO.',
-						locked : true,
-						width : 28
-					});
+	// 定义Checkbox
+	var sm = new Ext.grid.CheckboxSelectionModel({
+				singleSelect : true
+			});
+	// 定义自动当前页行号
+	var rm = new Ext.grid.RowNumberer({
+				header : 'NO.',
+				locked : true,
+				width : 28
+			});
 
-			// 定义锁定列模型
-			var cm = new Ext.grid.ColumnModel([{
-						header : '回复内容',
-						dataIndex : 'reply',
-						sortable : true,
-						width : 450
-					}, {
-						header : '系统评分',
-						dataIndex : 'score',
-						sortable : true,
-						width : 100
-					}]);
+	// 定义锁定列模型
+	var cm = new Ext.grid.ColumnModel([{
+				header : '回复内容',
+				dataIndex : 'reply',
+				sortable : true,
+				width : 450
+			}, {
+				header : '系统评分',
+				dataIndex : 'score',
+				sortable : true,
+				width : 100
+			}]);
 
-			// 定义grid表格
-			var grid = new Ext.grid.GridPanel({
-						region : 'center',
-						id : 'grid',
-						stripeRows : true, // 斑马线
-						frame : true,
-						title : '回复搜索',
-						split : true,
-						autoScroll : true,
-						store : store,
-						loadMask : {
-							msg : '正在加载表格数据,请稍等...'
-						},
-						colModel : cm,
-						tbar : [{
-									text : '刷新',
-									iconCls : 'arrow_refreshIcon',
-									handler : function() {
-										store.reload();
-									}
-								}, '-', new Ext.form.TextField({
-											id : 'weibo',
-											name : 'weibo',
-											fieldLabel : '微博',
-											emptyText : '请输入微博内容',
-											allowBlank : false,
-											enableKeyEvents : true,
-											width : 130
-										}), '-', new Ext.form.TextField({
-											id : 'rows',
-											name : 'rows',
-											fieldLabel : '显示的行数',
-											emptyText : '10',
-											enableKeyEvents : true,
-											width : 130
-										}), '-', {
-									text : '查询记录',
-									iconCls : 'page_findIcon',
-									handler : function() {
-										var weibo = Ext.getCmp('weibo')
-												.getValue();
-										var rows = Ext.getCmp('rows')
-												.getValue();
-										if (Ext.isEmpty(weibo)) {
-											Ext.Msg.alert("提示", "请输入要搜索的内容");
-											return;
-										}
-										store.setBaseParam('weibo', weibo);
-										store.setBaseParam('rows', rows);
-										store.load();
-									}
-								}]
-					});
+	// 定义grid表格
+	var grid = new Ext.grid.GridPanel({
+				region : 'center',
+				id : 'grid',
+				stripeRows : true, // 斑马线
+				frame : true,
+				title : '回复搜索',
+				split : true,
+				autoScroll : true,
+				store : store,
+				loadMask : {
+					msg : '正在加载表格数据,请稍等...'
+				},
+				colModel : cm,
+				tbar : [{
+							text : '刷新',
+							iconCls : 'arrow_refreshIcon',
+							handler : function() {
+								store.reload();
+							}
+						}, '-', new Ext.form.TextField({
+									id : 'weibo',
+									name : 'weibo',
+									fieldLabel : '微博',
+									emptyText : '请输入微博内容',
+									allowBlank : false,
+									enableKeyEvents : true,
+									width : 130
+								}), '-', new Ext.form.TextField({
+									id : 'rows',
+									name : 'rows',
+									fieldLabel : '显示的行数',
+									emptyText : '10',
+									enableKeyEvents : true,
+									width : 130
+								}), '-', {
+							text : '查询记录',
+							iconCls : 'page_findIcon',
+							handler : function() {
+								var weibo = Ext.getCmp('weibo').getValue();
+								var rows = Ext.getCmp('rows').getValue();
+								if (Ext.isEmpty(weibo)) {
+									Ext.Msg.alert("提示", "请输入要搜索的内容");
+									return;
+								}
+								store.setBaseParam('weibo', weibo);
+								store.setBaseParam('rows', rows);
+								store.load();
+							}
+						}, '-', new Ext.form.TextField({
+									id : 'cookies',
+									name : 'cookies',
+									fieldLabel : 'cookies',
+									emptyText : '输入cookies',
+									enableKeyEvents : true,
+									width : 200
+								}), '-', {
+							text : '设置cookies',
+							iconCls : 'page_findIcon',
+							handler : function() {
+								var cookies = Ext.getCmp('cookies').getValue();
+								if (Ext.isEmpty(cookies)) {
+									Ext.Msg.alert("提示", "请输入cookies");
+									return;
+								}
+								// TODO 数据访问
+								Ext.Ajax.request({
+											url : '../reply/cookies?_time'
+													+ new Date().getTime(),
+											params : {
+												cookies : cookies
+											},
+											success : function(response) {
+												var result = Ext
+														.decode(response.responseText);
+												if (result.success) {
+													Ext.Msg.alert("提示", "设置成功");
+												} else {
+													Ext.Msg.alert("提示", "设置失败");
+												}
+											},
+											failure : function() {
+												Ext.Msg.alert("提示", "设置失败");
+											}
+										});
+								Ext.getCmp('cookies').setValue(null);
+								return;
+							}
+						}]
+			});
 
-			// 布局模型
-			var viewport = new Ext.Viewport({
-						layout : 'border',
-						items : [grid]
-					});
-		});
+	// 布局模型
+	var viewport = new Ext.Viewport({
+				layout : 'border',
+				items : [grid]
+			});
+});
--- trunk/spreader-backend/src/com/nali/spreader/controller/ReplySearchController.java
+++ trunk/spreader-backend/src/com/nali/spreader/controller/ReplySearchController.java
@@ -6,23 +6,30 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.nali.common.util.CollectionUtils;
 import com.nali.spreader.controller.basectrl.BaseController;
 import com.nali.spreader.data.ReplySearch;
 import com.nali.spreader.lucene.service.ISearchService;
 import com.nali.spreader.remote.ISegmenAnalyzerService;
+import com.nali.spreader.spider.service.ICommentsService;
 
 @Controller
 @RequestMapping(value = "/reply")
 public class ReplySearchController extends BaseController {
+	private static final Logger logger = Logger
+			.getLogger(ReplySearchController.class);
 	@Autowired
 	private ISegmenAnalyzerService segmenAnalyzerService;
 	@Autowired
 	private ISearchService searchService;
+	@Autowired
+	private ICommentsService commentsService;
 
 	@Override
 	public String init() {
@@ -45,5 +52,25 @@ public class ReplySearchController extends BaseController {
 		}
 		m.put("list", list);
 		return write(m);
+	}
+
+	/**
+	 * 设置cookies
+	 * 
+	 * @param cookies
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/cookies")
+	public String settingCookies(String cookies) {
+		Map<String, Boolean> map = CollectionUtils.newHashMap(1);
+		map.put("success", false);
+		try {
+			commentsService.settingCookies(cookies);
+			map.put("success", true);
+		} catch (Exception e) {
+			logger.error("settingCookies error", e);
+		}
+		return write(map);
 	}
 }
