2534fee65544af6428aede97bfdf24af36ccbb89
eixie9 <eixie9@5a2aac17-e56e-ac70-9358-26bf2cb0f114>
2013-03-08 08:29:00 +0000

    <add>客户端配置DAO及Service

---
+++ trunk/spreader-action-config/src/com/nali/spreader/service/IClitentConfigService.java
@@ -0,0 +1,43 @@
+package com.nali.spreader.service;
+
+import com.nali.common.model.Limit;
+import com.nali.common.pagination.PageResult;
+import com.nali.spreader.model.ClientConfig;
+
+public interface IClitentConfigService {
+	/**
+	 * 分页查询客户端配置信息
+	 * 
+	 * @param clientId
+	 * @param limit
+	 * @return
+	 */
+	PageResult<ClientConfig> queryPageData(Long clientId, Limit limit);
+
+	/**
+	 * 保存一条配置
+	 * 
+	 * @param id
+	 * @param clientId
+	 * @param cfgs
+	 */
+	void saveClientConfigs(Long id, Long clientId, String configName,
+			int configType, String cfgs, String cfgMD5);
+
+	/**
+	 * 根据ID获取配置对象
+	 * 
+	 * @param clientId
+	 * @return
+	 */
+	ClientConfig getConfigByClientId(Long clientId, String configName);
+
+	/**
+	 * 获取配置对象
+	 * 
+	 * @param id
+	 * @return
+	 */
+	ClientConfig getConfigById(Long id);
+
+}
