137fa0e5cce569f68249eda9b640200cb5b5c827
eixie9 <eixie9@5a2aac17-e56e-ac70-9358-26bf2cb0f114>
2013-03-15 07:31:17 +0000

    <add>增加24小时曲线

--- spreader-action-config/src/com/nali/spreader/service/DownSettingService.java
+++ spreader-action-config/src/com/nali/spreader/service/DownSettingService.java
@@ -1,5 +1,7 @@
 package com.nali.spreader.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,5 +20,10 @@ public class DownSettingService implements IDownSettingService {
 	@Override
 	public void setMaxDownloadPerHour(int id, int count) {
 		actionCooldownConfigDao.saveMaxDownloadPerHour(id, count);
+	}
+
+	@Override
+	public void setDownloadRate(int id, List<Double> rates) {
+		actionCooldownConfigDao.saveDownloadRate(id, rates);
 	}
 }
