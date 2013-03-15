6ad7c769f2e84b4975418deceb670604f635d161
eixie9 <eixie9@5a2aac17-e56e-ac70-9358-26bf2cb0f114>
2013-03-15 07:31:17 +0000

    <add>增加24小时曲线

--- trunk/spreader-action-config/src/com/nali/spreader/service/IDownSettingService.java
+++ trunk/spreader-action-config/src/com/nali/spreader/service/IDownSettingService.java
@@ -1,9 +1,12 @@
 package com.nali.spreader.service;
 
+import java.util.List;
+
 public interface IDownSettingService {
 
 	int getMaxDownloadPerHour(int id);
 
 	void setMaxDownloadPerHour(int id, int count);
 
+	void setDownloadRate(int id, List<Double> rates);
 }
