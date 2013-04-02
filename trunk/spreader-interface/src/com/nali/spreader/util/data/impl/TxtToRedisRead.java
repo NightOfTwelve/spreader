e943bf65feacdf9d9ed513659ea931b11f8c8078
czysay@gmail.com <czysay@gmail.com@5a2aac17-e56e-ac70-9358-26bf2cb0f114>
2013-04-02 03:43:35 +0000

    [fix]

--- trunk/spreader-interface/src/com/nali/spreader/util/data/impl/TxtToRedisRead.java
+++ trunk/spreader-interface/src/com/nali/spreader/util/data/impl/TxtToRedisRead.java
@@ -16,9 +16,9 @@ public class TxtToRedisRead<V> {
 
 	public TxtToRedisRead(String redisPrefix, String txtDir, RedisTemplate<String, Object> redisTemplate, Converter<String, V> valueConverter) {
 		TxtToRedisParameterConverters converters = new TxtToRedisParameterConverters(redisPrefix, txtDir);
-		RedisWrite<String, V> innerRedisWrites = new RedisWrite<String, V>(redisTemplate);
-		writes = new RawWrites<String,V>(new WriteWrap<String, V, String, V>(innerRedisWrites, converters.pToCp, null));
+		RedisWrite<String, V> redisWrite = new RedisWrite<String, V>(redisTemplate);
+		writes = new RawWrites<String,V>(new WriteWrap<String, V, String, V>(redisWrite, converters.pToCp, null));
 		TxtReads<V> txtReads = new TxtReads<V>(valueConverter);
-		reads = new CommonCachedReads.SameValue<String, URL, String, V>(converters.pToOp, converters.pToCp, txtReads, writes);
+		reads = new CommonCachedReads.SameValue<String, URL, String, V>(converters.pToOp, converters.pToCp, txtReads, new RawWrites<String,V>(redisWrite));
 	}
 }
