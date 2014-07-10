bcc59732fd99eff4fd0e7987677c86c58ee6e8ad
zfang442872966@gmail.com <zfang442872966@gmail.com@5a2aac17-e56e-ac70-9358-26bf2cb0f114>
2014-07-10 03:14:08 +0000

    单元测试代码

--- trunk/spreader-front/test/com/nali/spreader/front/ximalaya/TestRobotRemoteService.java
+++ trunk/spreader-front/test/com/nali/spreader/front/ximalaya/TestRobotRemoteService.java
@@ -3,7 +3,6 @@ package com.nali.spreader.front.ximalaya;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -31,25 +30,23 @@ public class TestRobotRemoteService {
 
 	@Test
 	public void test() throws AuthenticationException, NoSuchAlgorithmException, IOException {
-//		Map<String, Object> param = new HashMap<String, Object>();
-//		param.put("keyword", "xx");
-//		param.put("offset", 10);
-//		param.put("limit", 100);
-//		param.put("fansGte", 10L);
-//		param.put("fansLte", 100L);
-//		param.put("vType", 1);
-//		param.put("startCreateTime", new Date().getTime());
-//		param.put("endCreateTime", new Date().getTime());
-//		param.put("startUpdateTime", new Date().getTime());
-//		param.put("endUpdateTime", new Date().getTime());
-//		System.out.println("开始。。。。");
-//		mtd.execute(param, new HashMap<String, Object>(), 1L);
+		// Map<String, Object> param = new HashMap<String, Object>();
+		// param.put("keyword", "xx");
+		// param.put("offset", 10);
+		// param.put("limit", 100);
+		// param.put("fansGte", 10L);
+		// param.put("fansLte", 100L);
+		// param.put("vType", 1);
+		// param.put("startCreateTime", new Date().getTime());
+		// param.put("endCreateTime", new Date().getTime());
+		// param.put("startUpdateTime", new Date().getTime());
+		// param.put("endUpdateTime", new Date().getTime());
+		// System.out.println("开始。。。。");
+		// mtd.execute(param, new HashMap<String, Object>(), 1L);
 		Date sd1 = DateUtils.addDays(new Date(), -100);
 		Date ed1 = new Date();
-		byte[] md = interfaceCheckService.getParamsMD5(new Object[] { null, 0, 10, null, null,
-				null, sd1, ed1, null, null });
-		List<Map<String, Object>> list = robotRemoteService.queryUser(null, 0, 10, null, null,
-				null, sd1, ed1, null, null, md);
+		byte[] md = interfaceCheckService.getParamsMD5(new Object[] { null, 0, 10, null, null, null, sd1, ed1, null, null });
+		List<Map<String, Object>> list = robotRemoteService.queryUser(null, 0, 10, null, null, null, sd1, ed1, null, null, md);
 		System.out.println(list.toString());
 	}
 
