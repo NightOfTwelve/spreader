11cc0fc3aadc19607516a51156d9e0d0d34e26ba
zfang442872966 <zfang442872966@gmail.com>
2014-07-14 09:35:16 +0000

    修改邮件标题

--- spreader-client/src/com/nali/spreader/client/ximalaya/CheckRobotRemoteServiceActionMethod.java
+++ spreader-client/src/com/nali/spreader/client/ximalaya/CheckRobotRemoteServiceActionMethod.java
@@ -42,19 +42,19 @@ public class CheckRobotRemoteServiceActionMethod implements ActionMethod {
 			logger.info("robotRemoteService.queryUser list.size:" + list.size());
 
 			if (list == null || list.size() == 0) {
-				sendMail(mail, "[警告]调用robotRemoteService接口报警", "http://robot.ximalaya.com/robot/hessian/robotRemoteService queryUser接口返回数据为空");
+				sendMail(mail, "[警告][营销系统-主动关注]调用robotRemoteService接口报警", "http://robot.ximalaya.com/robot/hessian/robotRemoteService queryUser接口返回数据为空");
 			} else {
 				if (sendSuccessEmail) {
-					sendMail(mail, "[正常]调用robotRemoteService接口正常", "http://robot.ximalaya.com/robot/hessian/robotRemoteService queryUser接口调用正常  list.size:" + list.size());
+					sendMail(mail, "[正常][营销系统-主动关注]调用robotRemoteService接口正常", "http://robot.ximalaya.com/robot/hessian/robotRemoteService queryUser接口调用正常  list.size:" + list.size());
 				}
 				return true;
 			}
 		} catch (AuthenticationException e) {
-			sendMail(mail, "[错误]调用robotRemoteService接口错误", "http://robot.ximalaya.com/robot/hessian/robotRemoteService queryUser接口错误 异常：AuthenticationException\n" + e);
+			sendMail(mail, "[错误][营销系统-主动关注]调用robotRemoteService接口错误", "http://robot.ximalaya.com/robot/hessian/robotRemoteService queryUser接口错误 异常：AuthenticationException\n" + e);
 			logger.error(e, e);
 			throw new RuntimeException(e);
 		} catch (Exception e) {
-			sendMail(mail, "[错误]调用robotRemoteService接口错误", "http://robot.ximalaya.com/robot/hessian/robotRemoteService queryUser接口错误 异常：Exception\n" + e);
+			sendMail(mail, "[错误][营销系统-主动关注]调用robotRemoteService接口错误", "http://robot.ximalaya.com/robot/hessian/robotRemoteService queryUser接口错误 异常：Exception\n" + e);
 			logger.error(e, e);
 			throw new RuntimeException(e);
 		}
@@ -68,7 +68,7 @@ public class CheckRobotRemoteServiceActionMethod implements ActionMethod {
 	 */
 	private void sendMail(String mail, String title, String text) {
 		logger.info("sendMail() start...");
-		String[] eMail = mail.split(",");
+		String[] eMail = mail.trim().split(",");
 		SimpleMailMessage msg = new SimpleMailMessage();
 		msg.setFrom("spreader@ximalaya.com");
 		msg.setTo(eMail);
