8618a850d793a8f63a8a6de35a922bd8e1b8d29d
zfang442872966 <zfang442872966@gmail.com>
2014-08-27 08:25:58 +0000

    喜马拉雅任务提醒任务增加刷新按钮

--- spreader-backend/WebContent/js/app/main/centertabpanelcp.js
+++ spreader-backend/WebContent/js/app/main/centertabpanelcp.js
@@ -7,7 +7,22 @@ var mainTabPanel = new Ext.TabPanel({
 		title : '喜马拉雅任务提醒',
 		id : 'frame1',
 		html : '<div id="taskManagerId">加载中,请稍候...</div>' + getXimalayaExecute(),
-		autoScroll : true
+		autoScroll : true,
+		tbar : [{
+			text : '刷新',
+			iconCls : 'arrow_refreshIcon',
+			handler : function() {
+				Ext.Ajax.request({
+					url : '../extutil/ximalayatask?_time=' + new Date().getTime(),
+					async : true,
+					success : function(response) {
+						Ext.select("#taskManagerId").update(Ext.decode(response.responseText).msg);
+					},
+					failure : function() {
+					}
+				});
+			}
+		}]
 	}
 });
 
